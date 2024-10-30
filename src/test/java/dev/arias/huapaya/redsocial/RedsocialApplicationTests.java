691ee5680ac7d5227b6d25fa6325ee55f7d5bbf3
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-10-29 22:06:02 -0500

    first commit

---
+++ src/test/java/dev/arias/huapaya/redsocial/RedsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arias.huapaya.redsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
