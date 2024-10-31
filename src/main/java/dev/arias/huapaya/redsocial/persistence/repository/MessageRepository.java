f76a0cdd5aa31eececacccc92a083d0b4a4452af
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-10-31 08:40:50 -0500

    feat: Added repositories

---
+++ src/main/java/dev/arias/huapaya/redsocial/persistence/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package dev.arias.huapaya.redsocial.persistence.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.arias.huapaya.redsocial.persistence.entity.MessageEntity;
+
+@Repository
+public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+
+}
