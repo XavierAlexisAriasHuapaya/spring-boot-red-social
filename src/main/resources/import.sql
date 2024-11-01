6acaa13f5413b2fd365e2bd48394e2a1cc198bf9
Arias Huapaya Xavier Alexis <xavieralexisariashuapaya@hotmail.com>
2024-11-01 00:38:33 -0500

    fix: import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,31 @@
 insert into modules (name, base_path, created_at, updated_at, status) values ('AUTHENTICATION', '/auth', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into modules (name, base_path, created_at, updated_at, status) values ('VALIDATE', '/auth', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into modules (name, base_path, created_at, updated_at, status) values ('USER', '/user', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into modules (name, base_path, created_at, updated_at, status) values ('CHAT', '/chat', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into modules (name, base_path, created_at, updated_at, status) values ('MESSAGE', '/message', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into modules (name, base_path, created_at, updated_at, status) values ('PUBLICATION', '/publication', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 
 insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('AUTHENTICATE', '/authenticate', 'POST', true, 1, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('VALIDATE', '/validate', 'POST', true, 2, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 
 insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('USER_CREATE', '', 'POST', false, 3, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
-insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('USER_UPDATE', '', 'PUT', false, 3, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('USER_UPDATE', '/[0-9]*', 'PUT', false, 3, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('USER_FIND_ONE', '/[0-9]*', 'GET', false, 3, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('USER_PAGINATION', '', 'GET', false, 3, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('CHAT_CREATE', '', 'POST', false, 4, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('CHAT_UPDATE', '/[0-9]*', 'PUT', false, 4, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('CHAT_ALL', '', 'GET', false, 4, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('MESSAGE_CREATE', '', 'POST', false, 5, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('MESSAGE_UPDATE', '/[0-9]*', 'PUT', false, 5, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('MESSAGE_ALL', '', 'GET', false, 5, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('MESSAGE_BY_CHAT', '/chat/[0-9]*', 'GET', false, 5, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('PUBLICATION_CREATE', '', 'POST', false, 6, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('PUBLICATION_UPDATE', '/[0-9]*', 'PUT', false, 6, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+insert into operations (name, path, http_method, permit_all, module_id, created_at, updated_at, status) values ('PUBLICATION_ALL', '', 'GET', false, 6, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
+
 
 INSERT INTO roles (description, created_at, updated_at, status) VALUES ('ADMINISTRATOR', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 INSERT INTO roles (description, created_at, updated_at, status) VALUES ('USER', '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
@@ -17,6 +34,16 @@ INSERT INTO permissions (rol_id, operation_id) VALUES (2, 3);
 INSERT INTO permissions (rol_id, operation_id) VALUES (2, 4);
 INSERT INTO permissions (rol_id, operation_id) VALUES (2, 5);
 INSERT INTO permissions (rol_id, operation_id) VALUES (2, 6);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 7);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 8);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 9);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 10);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 11);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 12);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 13);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 14);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 15);
+INSERT INTO permissions (rol_id, operation_id) VALUES (2, 16);
 
 insert into users (username, password, email, rol_id, created_at, updated_at, status) values ('alexis', '$2a$10$7.kqP/Rb5aFoYnaPXt7jXOzJMrvXiLfYP1d5YSjqEIkpELpjjYl8C', 'xavieralexisariashuapaya@hotmail.com', 2, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
 insert into users (username, password, email, rol_id, created_at, updated_at, status) values ('manuel', '$2a$10$7.kqP/Rb5aFoYnaPXt7jXOzJMrvXiLfYP1d5YSjqEIkpELpjjYl8C', 'xavieralexisariashuapaya1999@gmail.com', 2, '2024-10-31T14:30:28.380152', '2024-10-31T14:30:28.380152', true);
